da12a490ff2b849ea0ff99b7b164d884e9a162cf
Kiranvora17 <vorakiran79@gmail.com>
2024-03-20 00:23:47 +0530

    minor bug fixes

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -2,9 +2,7 @@ import { useCallback, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import AlbumsPage from "../../Pages/AlbumsPage";
 import { useParams } from "react-router";
-import { albumAction, recommendActions } from "../../store/AlbumActions";
-import { compose } from "redux";
-import { fetchData } from "../pageComponents/FetchRequests";
+import { albumAction } from "../../store/AlbumActions";
 
 const Album = () => {
   const params = useParams();
@@ -12,33 +10,29 @@ const Album = () => {
   const dispatch = useDispatch();
   const accessToken = localStorage.getItem("access_token");
 
-  // const fetchData = useCallback(
-  //   async (url, saveData) => {
-  //     setLoading(true);
-  //     const request = await fetch(url, {
-  //       method: "GET",
-  //       headers: {
-  //         Authorization: `Bearer ${accessToken}`,
-  //       },
-  //     });
+  const fetchData = useCallback(
+    async (url, saveData) => {
+      setLoading(true);
+      const request = await fetch(url, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
-  //     const response = await request.json();
-  //     await dispatch(saveData(response));
-  //     setLoading(false);
-  //   },
-  //   [params.albumId]
-  // );
+      const response = await request.json();
+      await dispatch(saveData(response));
+      setLoading(false);
+    },
+    [params.albumId]
+  );
 
   useEffect(() => {
     fetchData(
-      `https://api.spotify.com/v1/albums/${params.albumId}?market=IN`
-    ).then((data) => albumAction(data));
-
-    // fetchData(
-    //   `https://api.spotify.com/v1/albums/${params.albumId}?market=IN`,
-    //   albumAction,
-    //   "id"
-    // );
+      `https://api.spotify.com/v1/albums/${params.albumId}?market=IN`,
+      albumAction,
+      "id"
+    );
   }, [params.albumId]);
 
   if (!loading) {
