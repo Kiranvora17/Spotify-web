455785c0608a601a5f8ebaa2acb6249c2fec6499
kiran vora <kiran@kiran.kiran>
2024-04-17 00:58:55 +0530

    changing redirect uri

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -13,7 +13,7 @@ const getCode = async () => {
   const codeChallenge = base64encode(hashed);
 
   const clientId = "4382c1ae8960482f8056bd573eec9f4c";
-  const redirectUri = "http://localhost:3000/login";
+  const redirectUri = "https://spotify-web-drab.vercel.app/login";
 
   const scope =
     "user-read-private user-read-email user-read-recently-played user-top-read user-follow-read user-library-read playlist-read-private user-read-playback-state user-modify-playback-state streaming user-library-modify playlist-modify-private playlist-modify-public user-follow-modify";
@@ -39,7 +39,7 @@ const getToken = async (code) => {
   // stored in the previous step
   let codeVerifier = localStorage.getItem("code_verifier");
   const clientId = "4382c1ae8960482f8056bd573eec9f4c";
-  const redirectUri = "http://localhost:3000/login";
+  const redirectUri = "https://spotify-web-drab.vercel.app/login";
 
   const payload = {
     method: "POST",
@@ -62,7 +62,7 @@ const getToken = async (code) => {
   localStorage.setItem("access_token", response.access_token);
   localStorage.setItem("refresh_token", response.refresh_token);
 
-  window.location.href = "http://localhost:3000/";
+  window.location.href = "https://spotify-web-drab.vercel.app/";
 };
 
 const LoginPage = () => {
@@ -76,7 +76,7 @@ const LoginPage = () => {
     }
 
     if (Date.now() - timeStamp < 3500000) {
-      window.location.href = "http://localhost:3000/";
+      window.location.href = "https://spotify-web-drab.vercel.app/";
     }
   }, []);
 
